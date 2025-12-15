d2d054e52aa1f5b664cb4f4914d5b7025c098dd0
Grabiel Espinosa Izada <gespinosaizada03@gmail.com>
2025-12-15 20:15:51 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-5332219249-b4c32",
   "appId": "1:565119421018:web:1f1eebf744ca68e5472663",
+  "storageBucket": "studio-5332219249-b4c32.appspot.com",
   "apiKey": "AIzaSyCzAGF5biKUQm65rl42WwD2jiXNt1uIZts",
   "authDomain": "studio-5332219249-b4c32.firebaseapp.com",
   "measurementId": "",
