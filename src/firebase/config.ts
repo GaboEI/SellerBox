c924a43851d010f17f10dee662da5b850291f408
Grabiel Espinosa Izada <gespinosaizada03@gmail.com>
2025-12-15 19:54:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5332219249-b4c32",
+  "appId": "1:565119421018:web:1f1eebf744ca68e5472663",
+  "apiKey": "AIzaSyCzAGF5biKUQm65rl42WwD2jiXNt1uIZts",
+  "authDomain": "studio-5332219249-b4c32.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "565119421018"
+};
